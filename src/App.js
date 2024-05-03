c8a22cd6c646481dd32944e704c8043c217046bf
talented_vicky <victorotubure7@gmail.com>
2024-05-03 09:56:33 +0100

    finished the ui

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const friends = [
   {
     id: 118836,
@@ -19,11 +21,15 @@ const friends = [
   },
 ]
 export default function App() {
+
   return (
     <div className="app"> 
       <div className="sidebar">
         <FriendsList></FriendsList>
+        <AddFriend ></AddFriend>
+        <Button> Add Friend </Button>
       </div>
+      <SplitBill></SplitBill>
     </div>
     
   )
@@ -31,29 +37,22 @@ export default function App() {
 
 function FriendsList() {
   return (
-    <>
-      <ul>
-        {
-          friends.map(friend => (<Friend friend={friend} key={friend.id}></Friend>))
-        }
-      </ul>
-      <button className="button"> Add friend </button>
-    </>
+    <ul>
+      {
+        friends.map(friend => (<Friend friend={friend} key={friend.id}></Friend>))
+      }
+    </ul>
   )
 }
 
 function Friend({friend}) {
   let classname;
-  if(friend.balance > 0){
-    classname = "green"
-  } else if(friend.balance < 0){
-    classname = "red";
-  }
-
   let billStatement = `You and ${friend.name} are even`;
   if(friend.balance > 0){
-    billStatement = `You owe ${friend.name} ___`
+    classname = "green";
+    billStatement = `You owe ${friend.name} ___`;
   } else if(friend.balance < 0){
+    classname = "red";
     billStatement = `${friend.name} owes you ___`;
   }
   
@@ -64,7 +63,55 @@ function Friend({friend}) {
         <h3> {friend.name} </h3>
         <p className={classname}> {billStatement} </p>
       </div>
-      <button className="button"> select </button>
+      <Button> Select </Button>
     </li>
+  )
+}
+
+function AddFriend() {
+  const [text, setText] = useState(friends.name);
+  const [img, setImage] = useState(friends.image);
+  
+  return (
+    <form className="form-add-friend">
+      <label> 🤗 Friend name </label>
+      <input type="text" value={text} onChange={e => setText(e.target.value)} />
+      
+      <label> 📷 Image URL </label>
+      <input type="text" value={img} onChange={e => setImage(e.target.value)}/>
+      
+      <Button> Add </Button>
+    </form>
+  )
+}
+function SplitBill({friend}){
+  const [bill, setBill] = useState();
+  function handleSplit() {
+    //
+  }
+
+  return <form className="form-split-bill">
+    <h2> Split Bill </h2>
+    <label> 💸 Total Bill </label>
+    <input type="text" value={bill} onChange={e => e.target.value}/>
+
+    <label> 👨‍🔧 Your bill </label>
+    <input type="text" value={bill} onChange={e => e.target.value}/>
+    
+    <label> 🥰 `${3} s bill` </label>
+    <input type="text" disabled value={bill} onChange={e => e.target.value}/>
+    
+    <label> ✔ Who is paying the bill </label>
+    <select value={bill}>
+      <option value={"me"} onChange={e => setBill(e.target.value)}> </option>
+      <option value={"you"} onChange={e => setBill(e.target.value)}> </option>
+    </select>
+    <Button> Split bill </Button>
+  </form>
+}
+
+function Button({children}) {
+  return (
+    <button className="button"> {children} </button>
   )
 }
